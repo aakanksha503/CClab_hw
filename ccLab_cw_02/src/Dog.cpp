f2a489fcfcdd86f4ee2af6f88c24a0b899e96c7f
Aakanksha Aggarwal <30570663+aakanksha503@users.noreply.github.com>
2017-12-09 05:34:43 -0500

    final submission

--- ccLab_cw_02/src/Dog.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Dog.cpp
-//  ccLab_cw_02
-//
-//  Created by Aditya Aggarwal on 14/11/2017.
-//
-//
-
-#include "Dog.hpp"
--- ccLab_cw_02_bouncingBalls/src/Ball.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Ball.cpp
-//  ccLab_cw_02_bouncingBalls
-//
-//  Created by Aditya Aggarwal on 14/11/2017.
-//
-//
-
-#include "Ball.hpp"
